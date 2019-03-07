3f954de8327511b7394d7d7c35620e4b3f087530
george <geomarray@gmail.com>
2019-03-06 16:19:54 -0800

    testing with travis

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# soccer-league
-Code test application for WeConnect Recovery
+## WeConnect Soccer League Assignment
+
+### Author: George Raymond
+
+### Links and Resources
+
+[![Build Status](https://www.travis-ci.com/georgeraymond92/00-deployment.svg?branch=master)](https://www.travis-ci.com/georgeraymond92/00-deployment)
+* [repo](https://github.com/georgeraymond92/soccer-league)
+* [travis](https://www.travis-ci.com/georgeraymond92/00-deployment)
+
+## Description
+Soccer-League is a command line application that calulates league standings when given game results. To run the application you must have node.js installed on your machine. Before running the application you will want to have a `.txt` file handy with game results. If your do not have such a file you may find one in the assets directory of the application. When you run the application you will be prompted for the file location of your input file ( Example: `./assets/sample-input.txt`). After providing a valid path the application will create a `standings.txt` file next to the application directory on your machine. This is calculated according to the following rules:
+```txt
+Win: 3pts
+Tie: 1pt
+Loss: 0pts
+```
+
+#### Sample Input
+```txt
+Lions 3, Snakes 3
+Tarantulas 1, FC Awesome 0
+Lions 1, FC Awesome 1
+Tarantulas 3, Snakes 1
+Lions 4, Grouches 0
+```
+#### Sample Output
+```txt
+1. Tarantulas, 6pts
+2. Lions, 5pts
+3. FC Awesome, 1pts
+3. Snakes, 1pts
+4. Grouches, 0pts
+```
+
+### Setup
+* Install Dependencties: `npm i`
+
+#### Running the app
+* `npm start`
+* Endpoint: `/index.js`
+  * creates output file `standings.txt`
+
+#### Testing
+* `npm test`
+
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,1 +1,124 @@
 'use strict';
+
+const soccer = require('../lib/class');
+
+let unAlphaArray = [
+  ['C', 0],
+  ['B', 0],
+  ['A', 0]
+]
+
+let alphOutputArray = [
+  ['A', 0],
+  ['B', 0],
+  ['C', 0]
+]
+
+describe('Testing the Soccer class', () => {
+
+  // testing that the class was exported properly
+  test('Proof of life', () => {
+    expect(soccer.position).toEqual(0);
+  })
+
+  test('Should alphabetize the array', () => {
+    expect(soccer.alphabetize(unAlphaArray)).toEqual(alphOutputArray);
+  })
+
+  test('Should add a new line to the writeString', () => {
+    soccer.sortData.push(['team', 2])
+    soccer.addLine(0);
+    expect(soccer.writeString).toBeTruthy();
+  })
+
+  test('Position counter should have been incremented', () => {
+    expect(soccer.position).toEqual(1);
+    soccer.position = 0;
+  })
+
+  test('Should empty the orderTies array', () => {
+    soccer.orderedTies.push('some dummy data');
+    soccer.emptyOrderedTies();
+   expect(soccer.orderedTies).toEqual([]);
+  })
+
+  test('Should concat orderTies data into writeString', () => {
+    soccer.orderedTies = [['testTeam', 2]];
+    soccer.addTiesToWriteString();
+    expect(soccer.writeString).toBeTruthy();
+  })
+
+  test('Should empty the orderTies array', () => {
+    soccer.emptyOrderedTies();
+    expect(soccer.orderedTies).toEqual([]); 
+  })
+
+  test('should add an item to orderedTies', () => {
+    soccer.orderedTies = [];
+    soccer.addTieToList(0);
+    expect(soccer.orderedTies.length).toBe(1);
+  })
+
+  test('Should increment the position counter', () => {
+    soccer.position = 0;
+    soccer.incrementPostionCounter();
+    expect(soccer.position).toEqual(1);
+  })
+
+  test('Should sort the teams based on their league score', () => {
+    soccer.sortData = [['team2', 3],['team1', 0]];
+    soccer.sortTeamsByScore();
+    expect(soccer.sortData[1]).toEqual(['team2',3]);
+    soccer.sortData = [];
+  })
+
+  test('Should construct sortData out of standings', () => {
+    soccer.standings = {
+      team: 2,
+      team2: 0
+    };
+    soccer.makeSortable()
+    expect(soccer.sortData[0][0]).toEqual('team');
+
+  })
+
+  test('Should calulate the standings', () => {
+    soccer.teams = ['team1','team2','team1','team2'];
+    soccer.scores = [2,0,2,2];
+    soccer.standings = {
+      team1: 0,
+      team2: 0
+    }
+    soccer.calculateStanding();
+    expect(soccer.standings.team1).toEqual(4);
+    soccer.standings = {};
+  })
+
+  test('Should construct the teams object', () => {
+    soccer.createTeamsObject();
+    expect(soccer.standings.team1).toEqual(0);
+  })
+
+  test('shoult capture the file path and store it', () => {
+    soccer.captureFilePath('./this/is/a/test/path');
+    expect(soccer.filePath).toEqual('./this/is/a/test/path');
+  })
+
+  test('RegEx should capture the team names', () => {
+    soccer.fileString = 'Test Team Name 1, Another Test Name 2\nSuperTeam 3, EvenSupererTeam 3\n';
+    soccer.teams = [];
+    soccer.scores = [];
+    soccer.captureNamesAndScores();
+    expect(soccer.teams.length).toEqual(4);
+  })
+
+  test('RegEx should have captured the scores', () => {
+    expect(soccer.scores.length).toEqual(4);
+
+  })
+
+
+
+
+
+})
